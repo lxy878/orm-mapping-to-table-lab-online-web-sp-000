c3586c53ce80c690bdb61dc6fb58af4e493810b0
Xiaoyi Liu <lxy878@gmail.com>
2020-03-27 08:19:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,11 @@
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
-  
+  #  with DB[:conn]
+
+  def self.create_table
+
+
+    DB[:conn]
+  end
 end
