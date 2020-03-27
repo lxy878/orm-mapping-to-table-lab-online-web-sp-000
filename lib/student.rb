7356c9f4a8e82831bc9a9f5cbe08e0e601c4ebe5
Xiaoyi Liu <lxy878@gmail.com>
2020-03-27 08:52:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,8 +38,7 @@ class Student
     SQL
 
     DB[:conn].execute(sql, @name, @grade)
-    a = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
-    binding.pry
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
 end
