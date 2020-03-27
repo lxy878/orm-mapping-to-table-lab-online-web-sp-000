c89dc2e9f26bee57bb624020688f728dc8f26395
Xiaoyi Liu <lxy878@gmail.com>
2020-03-27 08:22:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,12 +2,13 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
+  attr_accessor :name, :grade, :id
   def initialize(name, grade, id=nil)
-
-
+    @name = name
+    @grade = grade
+    @id = id
   end
-  
+
   def self.create_table
 
 
